af8faec492a8ac645661b37f661a180a7fe1240d
Tanmay Sayare <tanmaysayare12345@gmail.com>
2024-10-05 09:58:07 +0000

    Bug fixed

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -2427,3 +2427,8 @@ Noida
 - Place: Natal, Brazil
 - Bio: Web developer PHP and Python
 - Github: [Ítalo Epifânio](https://github.com/itepifanio)
+
+#### Name: [Tanmay Sayare](https://github.com/itepifanio)
+- Place: India
+- Bio: Web developer and Python , Linux Lover
+- Github: [Tanmay Sayare](https://github.com/Tanmay)
---
+++ scripts/bash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Function to print TANMAY in one line
+print_TANMAY() {
+    echo "TTTTTTTTTTT     A         N       N     M       M     A         Y     Y"
+    echo "    T         A   A       NN      N     MM     MM    A   A        Y   Y "
+    echo "    T        AAAAAAA      N N     N     M M   M M   AAAAAAA        Y Y  "
+    echo "    T       A       A     N  N    N     M  M M  M  A       A        Y   "
+    echo "    T      A         A    N   N   N     M   M   M A         A       Y   "
+    echo "    T     A           A   N    N  N     M       M A           A     Y   "
+    echo "    T    A             A  N     N N     M       M A             A    Y   "
+}
+
+# Call the function to print TANMAY in one line
+print_TANMAY
